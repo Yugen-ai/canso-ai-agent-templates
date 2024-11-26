7189d110f953125081499ac32bf336da138f1345
Mahantesh <mahantesh.shivagunde@yugen.ai>
2024-11-26 18:15:42 +0530

    Load env variables from .env file

---
+++ {{cookiecutter.agent_name}}/src/main.py
@@ -0,0 +1,5 @@
+from dotenv import load_dotenv
+
+# This loads the environment variables from .env file. 
+# It is recommended to keep this as the first statement in main.py and not to be removed.
+load_dotenv()
